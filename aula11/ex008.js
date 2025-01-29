d2b34f56189da2475950d745e043846e2f680958
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-01-28 21:08:20 -0300

    commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+const vel = 35.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 50){
+    console.log('Você passou acima do limite de velocidade, MULTADO!!');
+}
+console.log('Dirija sempre utilizando o sinto de segurança')
