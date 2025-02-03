f3f2199c88ae6c4dd43d2529dcadf97b31cca4ae
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-02-03 16:48:01 -0300

    commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+let contador = 1
+while(contador <= 10){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
